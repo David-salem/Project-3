d5712317b0fb36e00644e7de572d74cdf63989f8
David <davidsalem37@gmail.com>
2022-06-06 19:39:13 +0300

    Mileston 1

---
+++ javascript.js
@@ -0,0 +1,27 @@
+const result = document.getElementById("result1")
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click', () => {
+
+      document.getElementById("spinner").classList.add("spinner-border");
+
+      const fetch_url = `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${result.value}&limit=10&exchange=NASDAQ`;
+
+      fetch(fetch_url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+
+        let result = "";
+        
+            data.forEach(myFunction);
+            document.getElementById("spinner").classList.remove("spinner-border");
+            document.getElementById("sample_div").innerHTML = result;
+
+            function myFunction(value) {
+                result += `<li><a href="/company.html?symbol=${value.symbol}" class="text-decoration-none font-weight-normal text-secondary fs-4 hover-overlay">${value.name} (${value.symbol})</a></li>`
+            }
+      });  
+});
+
