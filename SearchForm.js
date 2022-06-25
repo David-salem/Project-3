11bbe237ca55353bfd609004aab51859b80ad560
David <davidsalem37@gmail.com>
2022-06-26 00:08:40 +0300

    mileston 7

--- SearchForm.js
+++ SearchForm.js
@@ -1,77 +1,84 @@
 class SearchForm {
-  constructor (form) {
+  constructor(form) {
     this.form = form;
-}
+  }
 
-structure(){
- let input = document.createElement("input");
- let div = document.createElement("div");
- let btn = document.createElement("button");
- let icon = document.createElement("i");
- 
- form.appendChild(div);
- div.appendChild(input);
- div.appendChild(btn);
- btn.appendChild(icon);
- 
- div.classList.add("d-flex");
- btn.classList.add("btn-primary", "btn");
- input.classList.add("form-control");
- icon.classList.add("fa-search","fas");
- 
- btn.id = "btn";
- input.id = "input";
-};
+  structure() {
+    let input = document.createElement("input");
+    let div = document.createElement("div");
+    let btn = document.createElement("button");
+    let icon = document.createElement("i");
+
+    form.appendChild(div);
+    div.appendChild(input);
+    div.appendChild(btn);
+    btn.appendChild(icon);
+
+    div.classList.add("d-flex");
+    btn.classList.add("btn-primary", "btn");
+    input.classList.add("form-control");
+    icon.classList.add("fa-search", "fas");
+
+    btn.id = "btn";
+    input.id = "input";
+  };
 
 
 
   getResult() {
-      btn.addEventListener("click", async () => {
-          document.getElementById("sample_div").innerHTML = "";
-          document.getElementById("spinner").classList.add("spinner-border");
-          fetch (`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${input.value}&limit=10&exchange=NASDAQ`)
-          .then(response => response.json())
-          .then((data) => {
+    btn.addEventListener("click", async () => {
+      document.getElementById("sample_div").innerHTML = "";
+      document.getElementById("spinner").classList.add("spinner-border");
+      fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/search?query=${input.value}&limit=10&exchange=NASDAQ`)
+        .then(response => response.json())
+        .then((data) => {
 
-
-  
           let symbol = [];
-  
-        data.forEach(myFunction);
-  
-        function myFunction(value) {
-          symbol.push(value.symbol);
-        }
-  
-        if(symbol.length === 0)
-        {
-          document.getElementById("spinner").classList.remove("spinner-border");
-          document.getElementById("sample_div").innerHTML = `<strong class="d-flex justify-content-center">No Company Found, Try Again</strong>`;
-        }
-  
-        for (let i = 0; i < symbol.length; i++) {
-          fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${symbol[i]}`)
-            .then(resp => resp.json())
-            .then((data) => {
-              
-              document.getElementById("spinner").classList.remove("spinner-border");
-              
-              let  changesPercentage = (Math.round(data.profile.changesPercentage * 100) / 100).toFixed(2);
-              
-              let color = "";
-              let plus = "";
-              
-              if (data.profile.changesPercentage < 0) {
-                color = "red";
-              } else if (data.profile.changesPercentage > 0) {
-                color = "green";
-                plus = "+"
-              } else color = "black";
-              
-              document.getElementById("sample_div").innerHTML += `<li class="list-unstyled mt-2"><a href="/company.html?symbol=${data.symbol}" class="text-decoration-none font-weight-normal text-secondary fs-4 hover-overlay"><img src="${data.profile.image}" class="img-responsive rounded company_image"> ${data.profile.companyName} (${data.symbol}) <span style='color:${color}'>(${plus}${changesPercentage}%)</span></a></li>`;
-            })
-      }
+
+          data.forEach(myFunction);
+
+          function myFunction(value) {
+            symbol.push(value.symbol);
+          }
+
+          if (symbol.length === 0) {
+            document.getElementById("spinner").classList.remove("spinner-border");
+            document.getElementById("sample_div").innerHTML = `<strong class="d-flex justify-content-center">No Company Found, Try Again</strong>`;
+          }
+
+          for (let i = 0; i < symbol.length; i++) {
+            fetch(`https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${symbol[i]}`)
+              .then(resp => resp.json())
+              .then((data) => {
+
+                document.getElementById("spinner").classList.remove("spinner-border");
+
+                let changesPercentage = (Math.round(data.profile.changesPercentage * 100) / 100).toFixed(2);
+
+                let color = "";
+                let plus = "";
+
+                if (data.profile.changesPercentage < 0) {
+                  color = "red";
+                } else if (data.profile.changesPercentage > 0) {
+                  color = "green";
+                  plus = "+"
+                } else color = "black";
+
+                const highlightedName = data.profile.companyName.replace(
+                  new RegExp(input.value, 'gi'),
+                  (matchName) => `<mark class="text-secondary bg-warning">${matchName}</mark>`
+                );
+
+                const highlightedSymbol = data.symbol.replace(
+                  new RegExp(input.value, 'gi'),
+                  (matchSymbol) => `<mark class="text-secondary bg-warning">${matchSymbol}</mark>`
+                );
+
+                document.getElementById("sample_div").innerHTML += `<li class="list-unstyled mt-2"><a href="/company.html?symbol=${data.symbol}" class="text-decoration-none font-weight-normal text-secondary fs-4 hover-overlay"><img src="${data.profile.image}" class="img-responsive rounded company_image"> ${highlightedName} (${highlightedSymbol}) <span style='color:${color}'>(${plus}${changesPercentage}%)</span></a></li>`;
+              })
+          }
+        })
     })
-  })
   }
-}
+};
--- union.js
+++ union.js
@@ -1,6 +1,6 @@
 (async function union() {
-    const marqueei = new Marquee('https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=25f11a1bbb7de48a75b503a7f07f0c07');
-    marqueei.getData();
+    const infoMarquee = new Marquee('https://financialmodelingprep.com/api/v3/stock-screener?marketCapMoreThan=1000000000&betaMoreThan=1&volumeMoreThan=10000&sector=Technology&exchange=NASDAQ&dividendMoreThan=0&limit=100&apikey=25f11a1bbb7de48a75b503a7f07f0c07');
+    infoMarquee.getData();
 
     const mainForm = new SearchForm(document.getElementById("form"));
     mainForm.structure();
